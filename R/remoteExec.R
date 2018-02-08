fa4cf366b0597e44a0adc7304725bbacdb99f953
jasenfinch <jsf9@aber.ac.uk>
2018-02-08 18:32:18 +0000

    added remoteExec

---
+++ R/remoteExec.R
@@ -0,0 +1,23 @@
+#' remoteExec
+#' @description execute an R expression on a remote host.
+#' @export
+
+remoteExec <- function(user,host,expr){
+    if (is.expression(expr)) {
+        session <- str_c(user,'@',host) %>%
+            ssh_connect()
+        cmd <- expr %>% 
+            as.character() %>%
+            base64_encode()
+        cmd <- str_c('Rscript -e \'eval(parse(text = rawToChar(openssl::base64_decode(\"',cmd,'\"))))\' &> .tmp.txt; cat .tmp.txt; rm .tmp.txt')
+        out <- ssh_exec_internal(session,cmd,error = F) 
+        
+        out %>%
+            .$stdout %>%
+            rawToChar() %>%
+            cat()
+    } else {
+        stop('expr is not an expression')
+    }
+    
+}
